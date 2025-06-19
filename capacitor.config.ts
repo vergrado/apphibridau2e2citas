31fea2bb2e4cf61c17b47c54136b8f2cec7ca541
vergrado <vergrado@gmail.com>
2025-06-19 10:27:52 -0400

    versio inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.apphibridau2e2citas',
+  appName: 'apphibridau2e2citas',
+  webDir: 'www'
+};
+
+export default config;
